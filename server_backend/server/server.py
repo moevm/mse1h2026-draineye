08d498a08db72c50414270eac12365eb862636f2
tylerdurden2005 <matveev.nikita.2005@mail.ru>
2026-05-03 21:58:21 +0300

    Добавление аутентификации в ручки

--- server_backend/server/server.py
+++ server_backend/server/server.py
@@ -2,6 +2,9 @@ from server_backend.services.storage_service import StorageService
 from server_backend.imports import Optional, List
 from server_backend.imports import FastAPI, Depends, UploadFile, File, HTTPException, Form
 from server_backend.schemas.auth_request import RegisterRequest
+from server_backend.models import User
+from server_backend.server.auth import get_current_user, require_admin, require_inspector
+from server_backend.schemas import InspectionSchema
 
 server_app = FastAPI()
 
@@ -10,9 +13,12 @@ def create_storage_service() -> StorageService:
     return StorageService()
 
 '''endpoint для получения всех инспекций конкретного инженера'''
-@server_app.get("/inspections_by_user/{id}")
-def get_inspections(id: str, ss: StorageService = Depends(create_storage_service)):
-    result  = ss.get_inspections_by_engineer(id)
+@server_app.get("/my_inspections")
+def get_inspections(
+    user: User = Depends(require_inspector),
+    ss: StorageService = Depends(create_storage_service)
+):
+    result = ss.get_inspections_by_engineer(user.user_id)
     return result
 
 '''endpoint для добавления инспекции конкретного инженера'''
@@ -20,10 +26,13 @@ def get_inspections(id: str, ss: StorageService = Depends(create_storage_service
 async def create_inspection(
     inspection_json: str = Form(...),
     files: Optional[List[UploadFile]] = File(default=None),
+    user: User = Depends(require_inspector),
     ss: StorageService = Depends(create_storage_service)
 ):
     try:
-        result = await ss.create_inspection_with_photos(inspection_json, files)
+        inspection_schema = InspectionSchema.model_validate_json(inspection_json)
+        inspection_schema.engineer_id = user.user_id
+        result = await ss.create_inspection_with_photos(inspection_schema, files)
         return {
             "inspection_id": result["inspection_id"],
             "status": result["status"]
